e3a1f1ac48c484482f6f46758276c816c0d12ee8
Stefan Kroneis <skroneis@yahoo.com>
2017-02-22 09:54:14 +0100

    final

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
 var fs = require('fs');
 var async = require('async');
 var schedule = require('node-schedule');
-// var GpioStone = require('./gpio_stone_wp');
-// var gpioStone = new GpioStone();
-//var filename = "/var/log/openvpn-status.log";
-var filename = "./samples/1.txt"
+var GpioStone = require('./gpio_stone_wp');
+var gpioStone = new GpioStone();
+var filename = "/var/log/openvpn-status.log";
+//var filename = "./samples/1.txt"
 var delay = 2000; //2 seonds
 
+console.log(filename);
 /*****************************************************/
 var checkVpn = function (callback) {
     try {
-        var data = fs.readFileSync(filename).toString().split("\r");
+        
+        var data = fs.readFileSync(filename).toString().split("\n");
         var foundIndex = 0;
         data.forEach(function (line) {
-            line = line.replace("\n", "");
-            // console.log(line);
-            if (line.startsWith("10.8.0")) {
+            // line = line.replace("\n", "");
+            //console.log(line);
+            //console.log(line[0]);
+            if (line.startsWith("10.8.0.")) {
                 // console.log("FOUND!");
                 foundIndex++;
             }
         });
-
+        // console.log(foundIndex);
         //return result
         return callback(null, foundIndex > 0);
     }
@@ -34,25 +37,26 @@ var checkVpn = function (callback) {
 /*checkVpn(function (err, activeVpnConnection) {
     // console.log(activeVpnConnection);
     if (activeVpnConnection) {
-        console.log("YES");
+        console.log("VPN connection active!");
         gpioStone.setOn(gpioStone.LED_GREEN);
     }
     else {
-        console.log("NO");
+        console.log("NO VPN connection active.");
         gpioStone.setOff(gpioStone.LED_GREEN);
     }
 });*/
+
 async.forever(
     function (next) {
         checkVpn(function (err, activeVpnConnection) {
             // console.log(activeVpnConnection);
             if (activeVpnConnection) {
-                console.log("YES");
-                // gpioStone.setOn(gpioStone.LED_GREEN);
+                console.log("VPN connection active! " + new Date().toISOString());
+                gpioStone.setOn(gpioStone.LED_GREEN);
             }
             else {
-                console.log("NO");
-                // gpioStone.setOff(gpioStone.LED_GREEN);
+                console.log("NO VPN connection active. " + new Date().toISOString());
+                gpioStone.setOff(gpioStone.LED_GREEN);
             }
         });
         //Repeat after the delay
