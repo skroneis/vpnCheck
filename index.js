a3e8bcad0da4edd9ef50d0a98e9e34038a763212
skroneis <skroneis@yahoo.com>
2017-02-22 09:01:42 +0100

    :camel:

--- index.js
+++ index.js
@@ -1,23 +1,40 @@
 var fs = require('fs');
 var GpioStone = require('./gpio_stone_wp');
 var gpioStone = new GpioStone();
+var filename = "./samples/1.txt";
 
-var filename = "./samples/0.txt";
-var data = fs.readFileSync(filename).toString().split("\r");
-var foundIndex = 0;
-data.forEach(function (line) {
-    line = line.replace("\n", "");
-    // console.log(line);
-    if (line.startsWith("10.8.0")) {
-        // console.log("FOUND!");
-        foundIndex++;
+/*****************************************************/
+var test = function (callback) {
+    try {
+        var data = fs.readFileSync(filename).toString().split("\r");
+        var foundIndex = 0;
+        data.forEach(function (line) {
+            line = line.replace("\n", "");
+            // console.log(line);
+            if (line.startsWith("10.8.0")) {
+                // console.log("FOUND!");
+                foundIndex++;
+            }
+        });
+
+        //return result
+        return callback(null, foundIndex > 0);
+    }
+    catch (e) {
+        console.log(e);
+        return callback(e);
+    }
+}
+/*****************************************************/
+
+test(function (err, activeVpnConnection) {
+    // console.log(activeVpnConnection);
+    if (activeVpnConnection) {
+        console.log("YES");
+        gpioStone.setOn(gpioStone.LED_GREEN);
+    }
+    else {
+        console.log("NO");
+        gpioStone.setOff(gpioStone.LED_GREEN);
     }
 });
-
-console.log(foundIndex);
-if (foundIndex > 0) {
-    //TODO: turn LED ON...
-    gpioStone.setOn(gpioStone.LED_GREEN);
-}
-else
-    gpioStone.setOff(gpioStone.LED_GREEN);
